5f8fa2960688f05ba63db65ce9d061c4652f97a5
rpalmafr <rpalmafr@ou.edu>
2020-10-18 18:02:39 -0500

    First draft of Shiny Dashboard
    Initial draft of the shiny dashboard to explore COVID-19 & Twitter data.
    
    To initiate the dashboard, run the "app.R" script.

--- Dashboard/app.R
+++ Dashboard/app.R
@@ -2,6 +2,8 @@
 library(shiny)
 library(shinydashboard)
 library(ggplot2)
+library(reshape)
+library(wordcloud)
 
 # Defining work environments
 dashboard_location <- getwd()
@@ -27,23 +29,87 @@ server <- function(input, output){
   # Plotting Confirmed cases time series
   output$covid_timeline <- renderPlot({
 
+    states <- input$timeline_state
     timeline <- seq(from = input$timeline_date[1], to = input$timeline_date[2],
-              by = "day")
+                    by = "day")
     
-    state_data <- data[timeline %in% data$Date, input$timeline_state]
-    date_subset_data <- data[data$Date %in% timeline, 1]
+    if(length(states) == 1){
+      
+      # Subsetting data based on dates selected by user
+      state_data <- data[data$Date %in% timeline, input$timeline_state]
+      date_subset_data <- data[data$Date %in% timeline, 1]
+      
+      # Combining data into a single dataset
+      new_data <- as.data.frame(cbind(Date = as.Date(date_subset_data,
+                                                     origin = "1970-01-01"), State = state_data))
+      
+      # Plotting
+      ggplot(new_data, aes(x = as.Date(Date, origin = "1970-01-01"),
+                           y = State)) + geom_line() +
+        ggtitle(paste0("Daily confirmed cases in ", states)) +
+        xlab("Date") + ylab("Confirmed cases")
+      
+    } else if(length(states) > 1){
+      
+      # Subsetting data based on dates selected by user
+      state_data <- data[data$Date %in% timeline, input$timeline_state]
+      date_subset_data <- data[data$Date %in% timeline, 1]
+      
+      # Combining data into a single dataset
+      new_data <- as.data.frame(cbind(Date = date_subset_data, State = state_data))
+      colnames(new_data) <- c("Date", states)
+      
+      # Converting data from long to tall to plot multiple lines in a single plot
+      test_new_data <- melt(new_data, id = "Date")
+      colnames(test_new_data) <- c("Date", "States", "value")
+      # Plotting
+      ggplot(test_new_data, aes(x = Date, y = value, colour = States)) +
+        geom_line() +
+        ggtitle(paste0("Daily confirmed cases in chosen states")) +
+        xlab("Date") + ylab("Confirmed cases")
+      
+    }
+  })
+  
+  # Plotting Wordmap
+  output$wordmap <- renderPlot({
     
-    print(date_subset_data)
+    set.seed(50)
     
-    new_data <- as.data.frame(cbind(Date = date_subset_data, State = state_data))
     
-    print(new_data)
-    
-    ggplot(new_data, aes(x = as.Date(Date, origin = "1970-01-01"), 
-                         y = State)) + geom_line() +
-      ggtitle(paste0("Daily confirmed cases in ", input$timeline_state)) +
-      xlab("Date") + ylab("Confirmed cases")
+    if(input$remove_covid == TRUE){
+      
+      twitter_dataset <- data[, c(53:ncol(data))]
+      twitter_dataset <- twitter_dataset[, 
+                    -which(colnames(twitter_dataset) %in% c("covid", 
+                                                            "coronavirus",
+                                                            "covid19"))]
+      
+    } else {
+      
+      # Dataset with only words
+      twitter_dataset <- data[, c(53:ncol(data))]
+      
+    }
 
+    
+    # Defining timeframe to subset from data
+    timeline <- seq(from = input$wordmap_date[1], to = input$wordmap_date[2],
+                    by = "day")
+    
+    # Subsetting. 52 (date + 51 states -- we only want words)
+    # word_data <- twitter_dataset[data$Date %in% timeline, 1:input$numb_words]
+    word_data <- twitter_dataset[data$Date %in% timeline, 
+                                 sample(colnames(twitter_dataset), input$numb_words,
+                                        replace = FALSE)]
+    
+    # Now sum each column
+    words <- colnames(word_data)
+    word_sums <- colMeans(word_data)
+    
+    wordcloud(words = words, freq = word_sums, max.words = 411, random.order = FALSE,
+              colors = brewer.pal(8, "Dark2"))
+    
   })
   
 }
--- Dashboard/ui.R
+++ Dashboard/ui.R
@@ -13,10 +13,24 @@ body <- dashboardBody(
   ),
   # Row 1
   fluidRow(
-    box(plotOutput("covid_timeline", height = 250)),
-    # box(tableOutput("data")),
-    box(title = "Timeline inputs",
-        selectInput("timeline_state", "Choose a state:",
+    box(title = "Confirmed cases over time", solidHeader = TRUE,
+        collapsible = TRUE, status = "primary",
+        
+        plotOutput("covid_timeline", height = 300)),
+    
+    box(title = "Inputs", solidHeader = TRUE,
+        collapsible = TRUE, status = "primary", height = 362,
+        
+        helpText("This plot represents the daily confirmed",
+        "COVID-19 cases in each state plus the District of Columbia.",
+        "You may plot up to 5 states at the same time. To delete",
+        "a selected state, click on it and press 'backspace' on your",
+        "keyboard.",
+        "In addition, you can also modify the timeline between two",
+        "particular dates from 04/13/2020 to 09/19/2020 to investigate",
+        "changes over time"),
+        
+        selectizeInput("timeline_state", "Choose up to five states:",
                     c("Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado",
                       "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia",
                       "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky",
@@ -25,7 +39,9 @@ body <- dashboardBody(
                       "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota",
                       "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
                       "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington",
-                      "West Virginia", "Wisconsin", "Wyoming")),
+                      "West Virginia", "Wisconsin", "Wyoming"),
+                    options = list(maxItems = 5),
+                    selected = "Alabama"),
         
         dateRangeInput("timeline_date", "Choose date range:",
                        start = "2020-04-13",
@@ -34,8 +50,41 @@ body <- dashboardBody(
                        max = "2020-09-19",
                        format = "yy/mm/dd",
                        separator = " - "))
+  ),
+  
+  fluidRow(
+    box(title = "Twitter words frequency over time", solidHeader = TRUE,
+        collapsible = TRUE, status = "success",
+               
+        plotOutput("wordmap", height = 500)),
+  
+  box(title = "Inputs", solidHeader = TRUE,
+      collapsible = TRUE, status = "success", height = 560,
+      
+      helpText("This plot represents the most commonly used words in",
+               "COVID-19 related tweets averaged over time.",
+               "You may plot up to 411 words at the same time. They are drawn",
+               "in no particular order from the dataset.",
+               "In addition, you can also modify the timeline between two",
+               "particular dates from 04/13/2020 to 09/19/2020 to investigate",
+               "changes over time.",
+               "Lastly, you can remove COVID-19 references from the wordmap",
+               "(e.g. covid, covid19, coronavirus) from the wordmap."),
+      
+      numericInput("numb_words", "Number of words to populate map",
+                   min = 10, max = 411, value = 10),
+      
+      dateRangeInput("wordmap_date", "Choose date range:",
+                     start = "2020-04-13",
+                     end = "2020-09-19",
+                     min = "2020-04-13",
+                     max = "2020-09-19",
+                     format = "yy/mm/dd",
+                     separator = " - "),
+      
+      checkboxInput("remove_covid", "Remove COVID-19 references", value = FALSE))
+    )
   )
-)
 
 
 dashboardPage(skin = "black",
